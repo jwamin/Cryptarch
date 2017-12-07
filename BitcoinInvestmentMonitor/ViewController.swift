5b6f143a841f57cc2d3e108c2aa16f2752e838ad
Joss Manger <joss.manger@langlandusa.com>
2017-12-07 08:59:56 -0500

    Initial Commit

---
+++ BitcoinInvestmentMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinInvestmentMonitor
+//
+//  Created by Joss Manger on 12/7/17.
+//  Copyright © 2017 Joss Manger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
