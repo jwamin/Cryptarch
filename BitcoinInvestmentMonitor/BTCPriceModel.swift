624b0e96ac4c285826a49994d709abbda874348b
Joss Manger <Jwam3rd@gmail.com>
2017-12-23 12:36:43 +0000

    used dispatch queue to request multiple prices

--- BitcoinInvestmentMonitor/BTCPriceModel.swift
+++ BitcoinInvestmentMonitor/BTCPriceModel.swift
@@ -16,77 +16,80 @@ protocol BTCPriceDelegate {
 class BTCPriceModel: NSObject {
 
     var btcRate:Float
+    var cryptoRates:Dictionary<String,Float> = [:]
     var delegate:BTCPriceDelegate?
+    let dispatch_group: DispatchGroup = DispatchGroup()
+    
+    
     override init() {
         btcRate = 0.0
     }
     
-    
     func getUpdateBitcoinPrice(){
-        DispatchQueue.global(qos: .background).async {
-            let cburl = "https://api.coinbase.com/v2/prices/spot?currency=USD"
-            //"https://api.coindesk.com/v1/bpi/currentprice.json"
-            if let url = URL(string:cburl){
-                var errorPointer:Error?
-                let task = URLSession.shared.dataTask(with: url, completionHandler: {
-                    (data, response, error) in
-                    if let gotError = error{
-                        errorPointer = gotError
-                        print(errorPointer as Any)
-                        self.delegate?.displayError()
-                    } else {
-                        //let str = String.init(data: data!, encoding: .utf8)
-                        do{
-                            let dict = try JSONSerialization.jsonObject(with: data!, options: .init(rawValue: 0)) as! Dictionary<String,Any>
-                            print(dict)
-                            
-                            if let data = dict["data"] as? Dictionary<String,Any>{
-                            
-                                                                //print(usd)
-                                    if let rate = data["amount"] as? NSString{
-                                        self.btcRate = rate.floatValue
-                                        self.delegate?.updatedPrice()
-                                    } else {
-                                        print("fail at rate")
-                                    }
-                            }
-//                            if let bpi = dict["bpi"] as? Dictionary<String,Any>{
-//                                //print(bpi)
-//                                if let usd = bpi["USD"] as? Dictionary<String,Any> {
-//
-//                                    //print(usd)
-//                                    if let rate = usd["rate_float"]{
-//                                        self.btcRate = Float(rate as! Float)
-////                                        DispatchQueue.main.async {
-////                                            self.setBTCLabelString()
-////                                        }
-//                                        self.delegate?.updatedPrice()
-//                                    } else {
-//                                        print("fail at rate")
-//                                    }
-//
-//
-//                                } else {
-//                                    print("fail at USD")
-//                                }
-//                            } else {
-//                                print("fail at bpi")
-//                            }
-                        } catch {
-                            fatalError()
-                        }
-                        
-                    }
-                })
-                
-                task.resume()
-                //NSURLConnection.s
-            }
+        
+        //Dispatch queue multiple async tasks, finally return tuple
+        let polling:[CryptoTicker] = [.btc,.ltc]
+        
+        for ticker in polling{
+            
+            request(ticker:ticker)
+            
         }
+        
+        dispatch_group.notify(queue: .main, execute: {
+            print("tasks done",self.cryptoRates)
+            self.delegate?.updatedPrice()
+        })
+
         
         
         
     }
+    
+    
+    func request(ticker:CryptoTicker){
+        dispatch_group.enter()
+        let cburl = "https://api.coinbase.com/v2/prices/"+ticker.stringValue()+"-USD/spot"
+        //"https://api.coindesk.com/v1/bpi/currentprice.json"
+        if let url = URL(string:cburl){
+            var errorPointer:Error?
+            let task = URLSession.shared.dataTask(with: url, completionHandler: {
+                (data, response, error) in
+                if let gotError = error{
+                    errorPointer = gotError
+                    print(errorPointer as Any)
+                    self.delegate?.displayError()
+                } else {
+                    
+                    do{
+                        let dict = try JSONSerialization.jsonObject(with: data!, options: .init(rawValue: 0)) as! Dictionary<String,Any>
+                        print(dict)
+                        
+                        if let data = dict["data"] as? Dictionary<String,Any>{
+                            
+                            //print(usd)
+                            if let rate = data["amount"] as? NSString{
+                                if(ticker == .btc){
+                                    self.btcRate = rate.floatValue
+                                }
+                                self.cryptoRates[ticker.stringValue()] = rate.floatValue
+                                self.dispatch_group.leave()
+                            } else {
+                                print("fail at rate")
+                            }
+                        }
+                        
+                    } catch {
+                        fatalError()
+                    }
+                    
+                }
+            })
+            
+            task.resume()
+        }
+    }
+    
     
     func processInfo(buy:Buy) -> Dictionary<String,String>{
         
@@ -104,7 +107,7 @@ class BTCPriceModel: NSObject {
         buyDict["priceAtBuy"] = String(originalPrice)
         buyDict["currentRate"] = String(btcRate)
         buyDict["currency"] = buy.cryptoCurrency
-        print(buy.cryptoCurrency,CryptoTicker.ticker(ticker: buy.cryptoCurrency))
+        //print(buy.cryptoCurrency,CryptoTicker.ticker(ticker: buy.cryptoCurrency))
         buyDict["currentPrice"] = String(format: "%.2f", arguments: [currentPrice])
         buyDict["appreciation"] = String(format: "%.2f", arguments:[(actualDecimal * 100)])+"%";
         buyDict["direction"] = (appreciationDecimal>1) ? "up" : "down"
